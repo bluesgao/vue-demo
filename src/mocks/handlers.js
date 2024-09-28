5fd8f38ea817d0b709d983f4274bae45d4ad0e9e
gocode <gocode@PCADMs-Mac-mini.local>
2024-09-28 17:14:03 +0800

    mock

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw'
- 
+import {faker} from '@faker-js/faker/locale/zh_CN'
+
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
   http.get('https://example.com/user', () => {
@@ -7,8 +8,8 @@ export const handlers = [
 
     return HttpResponse.json({
       id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
     })
   }),
 ]
